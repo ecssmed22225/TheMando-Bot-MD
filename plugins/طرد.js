4fb8ece0611cfe0432062d1507db29555706b43a
ecssmed22225 <emailgeded5@gmail.com>
2024-10-08 21:45:08 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -7,12 +7,12 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*تـــم طردك من النقابة بواسطة فلاش بوت تعالي بوس حذائي عشان ارجعك !*`) 
+m.reply(`*تـــم طردك من النقابة بواسطة ماندو بوت تعالي بوس زبي عشان ارجعك !*`) 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'طرد'] 
+handler.command = ['طرد', 'كسمك'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
